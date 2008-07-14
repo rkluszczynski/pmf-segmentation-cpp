07103790ac94e3729fc23a894e95801fb22a963d
rkluszczynski <klusii@icm.edu.pl>
2008-07-14 12:00:01 +0000

    utworzenie pliku z metoda realizujaca symulacje dla punktow-krawedzie poprawienie testujacego pliku tests/test-edgepoints.cpp

---
+++ src/sim-edge.cpp
@@ -0,0 +1,21 @@
+
+#include "PMF.hpp"
+#include "edgepoints.hpp"
+
+void SimulateAddingEdges (
+                            char * edgesFile,
+                            PMF<double> * pmf
+                        )
+{
+    EdgePoints<double> ep;
+    ep.LoadData(edgesFile);
+    ep.PrintData();
+
+
+    PMF<double> * pmf2 = pmf->Clone();
+    pmf2->SaveConfiguration("output/pmf2.txt");
+    delete pmf2;
+
+
+    return;
+}
