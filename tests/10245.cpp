1a1c453d944bb957e3fcb13d15dbca1f2bd8386f
rkluszczynski <klusii@icm.edu.pl>
2008-12-28 13:48:14 +0000

    wstepna implementacja metody zamiatania

--- tests/10245.cpp
+++ tests/10245.cpp
@@ -3,16 +3,26 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <map>
 
 #include "avl.hpp"
 
 using namespace std;
 
-#include <map>
+
+std::ostream & operator << (std::ostream& s, const pair<int,int> & p)
+{
+    s << "( " << p.first << " , " << p.second << " )";
+    return s;
+}
+
+
 class ClosestPair
 {
     private :
         vector<pair<int, int> > _pts;
+        double distance;
+        pair<int, int> P, Q;
 
         struct comp_map {
             bool operator() (const int& lhs, const int& rhs) const { return lhs<rhs; }
@@ -20,79 +30,91 @@ class ClosestPair
 
 
     public :
-        ClosestPair(int n = 0) : _pts(n) { }
+        ClosestPair(int n = 0) : _pts(n), distance(-1) { }
+        ~ClosestPair() { }
+
+        pair<int, int> & operator[] (const int index) { return _pts[index]; }
 
         void PushBack(pair<int, int> & pp) { _pts.push_back(pp); }
         void SortPoints() { sort(_pts.begin(), _pts.end()); }
         void Execute()
         {
-            map<int, pair<int, int> > pion;
+#define DIST2(P1, P2) ((P1.second-P2.second)*(P1.second-P2.second)+(P1.first-P2.first)*(P1.first-P2.first))
+            map<int, pair<int, int>, comp_map> pion;
+
+            //for (int i = 0; i < _pts.size(); i++) pion.insert( pair<int, pair<int, int> >(_pts[i].second, _pts[i]));
 
             int current = 0;
             int firstActive = current;
-            double delta = 100000000;
-            pair<int, int> P, Q;
+            double delta = 10000;
 
             while (current < _pts.size())
             {
                 pair<int, int> p = _pts[current];
                 current++;
 
+                /*/ print content:
+                map<int, pair<int, int> >::iterator it;
+                cout << "[ MAP ] ~ " << current << " : " << endl;
+                for ( it=pion.begin() ; it != pion.end(); it++ )
+                    cout << (*it).first << " => " << (*it).second << endl;
+                cout << "\\___" << endl;
+                //*/
                 map<int, pair<int, int> >::iterator itlow, itup;
 
                 itlow = pion.lower_bound(p.second);
                 itup = pion.upper_bound(p.second);
 
                 int i = 0;
-                /*
-                while (r  &&  i < 4) {
-                    if (sqrt(DIST2(p, r->key)) < delta) {
-                        cout << "... changing distance : " << p << " and " << r->key << endl;
-                        delta = sqrt(DIST2(p, r->key));
+                while (itlow != pion.end() and i < 4)
+                {
+                    //cout << " " << itlow->second.first << "x" << itlow->second.second;
+                    if (sqrt(DIST2(p, itlow->second)) < delta) {
+                        cout << "... changing distance (L): " << p << " and " << itlow->second << endl;
+                        delta = sqrt(DIST2(p, itlow->second));
                         cout << "... changed to " << delta << endl;
-                        P = p;  Q = r->key;
+                        P = p;  Q = itlow->second;
                     }
-                    r = r->next;
+                    itlow--;
                     i++;
                 }
-                //*/
-            }
-/*
+                //cout << " ." << endl;
 
-
-            i = 0;
-            while (l  &&  i < 4) {
-                if (sqrt(DIST2(p, l->key)) < delta) {
-                    cout << "... changing distance : " << p << " and " << l->key << endl;
-                    delta = sqrt(DIST2(p, l->key));
-                    cout << "... changed to " << delta << endl;
-                    P = p;  Q = l->key;
+                i = 0;
+                while (itup != pion.end()  &&  i < 4) {
+                    if (sqrt(DIST2(p, itup->second)) < delta) {
+                        cout << "... changing distance (U): " << p << " and " << itup->second << endl;
+                        delta = sqrt(DIST2(p, itup->second));
+                        cout << "... changed to " << delta << endl;
+                        P = p;  Q = itup->second;
+                    }
+                    itup++;
+                    i++;
                 }
-                l = l->prev;
-                i++;
-            }
 
-            cout << " activeBefore = " << firstActive << endl;
-            int q = firstActive;
-            //double sqrt_delta = sqrt(delta);
-            while ((p.first-pts[q].first) > delta)
-            {
-                firstActive++;
-                avl.remove(pts[q]);
-                q = firstActive;
+                cout << " activeBefore = " << firstActive << endl;
+                int q = firstActive;
+                //double sqrt_delta = sqrt(delta);
+                while ((p.first-_pts[q].first) > delta)
+                {
+                    firstActive++;
+                    pion.erase(_pts[q].second);
+                    //avl.remove(pts[q]);
+                    q = firstActive;
+                }
+                pion.insert( pair<int, pair<int, int> >(p.second, p));
+                cout << " active After = " << firstActive << endl;
             }
-            avl.insert(p);
-
-            cout << " active After = " << firstActive << endl;
-            avl.print();
+#undef DIST2
+            distance = delta;
         }
 
-        avl.print();
-        cout << " RESULT = " << delta << "  ... by " << P << " and " << Q << endl;
-//*/
+        void Write()
+        {
+            printf("[ RESULT ] :  distance = %.6lf  ", distance);
+            printf("by  ( %i , %i )  ", P.first, P.second);
+            printf("and  ( %i , %i )\n", Q.first, Q.second);
         }
-
-        pair<int, int> & operator[] (const int index) { return _pts[index]; }
 };
 
 
@@ -113,112 +135,8 @@ int main()
         }
         cp.SortPoints();
         for (int i = 0; i < N; i++) cout << " " << cp[i].first << "x" << cp[i].second;  cout << endl;
-
-    }
-    return 0;
-}
-
-
-
-
-class PAVL : public AVL<pair<int, int> >
-{
-    private :
-        virtual inline bool __less_then (pair<int, int> p1, pair<int, int> p2)
-        {
-            return p1.second < p2.second;
-        };
-};
-
-std::ostream & operator << (std::ostream& s, const pair<int,int> & p)
-{
-    s << p.first << " x " << p.second;
-    return s;
-}
-
-
-//AVL<pair<int, int> > avl;
-vector<pair<int, int> > pts;
-
-#define DIST2(P1, P2) ((P1.second-P2.second)*(P1.second-P2.second)+(P1.first-P2.first)*(P1.first-P2.first))
-int main2()
-{
-    ifstream fin("tests/in10245.txt");  cin.rdbuf(fin.rdbuf());
-    int N;
-    while(true) {
-        cin >> N;
-        if (N == 0) break;
-
-        pts.clear();
-        for (int i = 0; i < N; i++) {
-            pair<int, int> pp;
-            cin >> pp.first;
-            cin >> pp.second;
-            pts.push_back(pp);
-        }
-        sort(pts.begin(), pts.end());
-        for (int i = 0; i < N; i++) cout << " " << pts[i].first << "x" << pts[i].second;  cout << endl;
-
-        PAVL avl;
-        int current = 0;
-        int firstActive = current;
-        double delta = 100000000;
-        pair<int, int> P, Q;
-
-        while (current < pts.size())
-        {
-            pair<int, int> p = pts[current];
-            current++;
-            Tnode<pair<int, int> > * l = avl.findLE(p);
-            Tnode<pair<int, int> > * r = (l) ? l->next : avl.get_min();
-
-            cout << " p = " << p.first << " x " << p.second << endl;
-            cout << " current = " << current << endl;
-            cout << " l = "; avl.print_node(l);
-            cout << " r = "; avl.print_node(r);
-            cout << " delta = " << delta << endl;
-
-            int i = 0;
-            while (r  &&  i < 4) {
-                if (sqrt(DIST2(p, r->key)) < delta) {
-                    cout << "... changing distance : " << p << " and " << r->key << endl;
-                    delta = sqrt(DIST2(p, r->key));
-                    cout << "... changed to " << delta << endl;
-                    P = p;  Q = r->key;
-                }
-                r = r->next;
-                i++;
-            }
-
-            i = 0;
-            while (l  &&  i < 4) {
-                if (sqrt(DIST2(p, l->key)) < delta) {
-                    cout << "... changing distance : " << p << " and " << l->key << endl;
-                    delta = sqrt(DIST2(p, l->key));
-                    cout << "... changed to " << delta << endl;
-                    P = p;  Q = l->key;
-                }
-                l = l->prev;
-                i++;
-            }
-
-            cout << " activeBefore = " << firstActive << endl;
-            int q = firstActive;
-            //double sqrt_delta = sqrt(delta);
-            while ((p.first-pts[q].first) > delta)
-            {
-                firstActive++;
-                avl.remove(pts[q]);
-                q = firstActive;
-            }
-            avl.insert(p);
-
-            cout << " active After = " << firstActive << endl;
-            avl.print();
-        }
-
-        avl.print();
-        cout << " RESULT = " << delta << "  ... by " << P << " and " << Q << endl;
+        cp.Execute();
+        cp.Write();
     }
     return 0;
 }
