6c9d52560bb5ae92eec5ea6923797afb65875704
rkluszczynski <klusii@icm.edu.pl>
2009-12-27 09:53:02 +0000

    w.i.p. (mosaic)

--- init/MosaicPMF.cpp
+++ init/MosaicPMF.cpp
@@ -15,6 +15,7 @@
 #define UNIFORM(A, B)   (((double(rand()) + 1.) / (double(RAND_MAX) + 2.)) * ((B) - (A)) + (A))
 
 
+
 MosaicPMF::MosaicPMF(double w, double h, unsigned int n) : fieldWidth(w), fieldHeight(h), linesNumber(n)
 {
     //ctor
@@ -43,6 +44,47 @@ MosaicPMF::MosaicPMF(double w, double h, unsigned int n) : fieldWidth(w), fieldH
     FOREACH(it, linesParameters)
     {
         cout << " ->   L = " << it->ST << "   d = " << it->ND << endl;
+
+        double a = - 1. / tan(it->ST);
+        double b;
+        if (it->ST < 0.  or  M_PI_2 < it->ST)  b = it->ND / cos(it->ST);
+        else  b = - it->ND / cos(it->ST);
+
+        printf("  >   y  =  %.2lf  *  x  +  %.2lf\n", a, b);
+
+        double tmpx, tmpy;
+        int check = 0;
+        // left
+        if (0. < b  and  b < h)
+        {
+            cout << " ( " << 0. << " , " << b << " )" << endl;
+            ++check;
+        }
+        // up
+        tmpx = (h - b) / a;
+        if (0. < tmpx  and  tmpx < w)
+        {
+            cout << " ( " << tmpx << " , " << h << " )" << endl;
+            ++check;
+        }
+        // right
+        tmpy = a * w + b;
+        if (0. < tmpy  and  tmpy < h)
+        {
+            cout << " ( " << w << " , " << tmpy << " )" << endl;
+            ++check;
+        }
+        // down
+        tmpx = - b / a;
+        if (0. < tmpx  and  tmpx < w)
+        {
+            cout << " ( " << tmpx << " , " << 0. << " )" << endl;
+            ++check;
+        }
+        //assert(check == 2);
+
+
+        cout << endl;
     }
 }
 
