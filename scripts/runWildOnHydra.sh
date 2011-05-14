83efbdef53ffbf04b8d0acc08a91f93f0fbd433c
rkluszczynski <klusii@icm.edu.pl>
2011-05-14 00:48:14 +0000

    minor changes

--- scripts/runWildOnHydra.sh
+++ scripts/runWildOnHydra.sh
@@ -3,7 +3,7 @@
 #	usage : $0 how_many_jobs how_many_threads
 #
 SAVECHECK=`find . -type d | wc -l`
-if [ $SAVECHECK -gt 1 ]; then
+if [ ${SAVECHECK} -gt 1 ]; then
 	echo "ERROR: some directory exist in cwd"
 	exit 1
 fi
@@ -11,27 +11,30 @@ fi
 HOW_MANY_JOBS=${1:-10}
 THREADS=${2:-4}
 
-CWD="`pwd`"
-JOBSLOG=$CWD/qsub.log
+QSUB_TEMPLATE=`dirname $0`/runOnHydra.pbs
 
-hostname > $JOBSLOG
-pwd >> $JOBSLOG
-date >> $JOBSLOG
-for i in `seq 1 $HOW_MANY_JOBS`
+CWD="`pwd`"
+JOBSLOG=${CWD}/qsub-${HOW_MANY_JOBS}x${THREADS}.log
+
+hostname > ${JOBSLOG}
+pwd >> ${JOBSLOG}
+date >> ${JOBSLOG}
+for i in `seq 1 ${HOW_MANY_JOBS}`
 do
-	echo "Number $i"
-	DIR="$CWD/sim-num-$i"
+	echo "Number ${i}"
+	DIR="$CWD/sim-num-${i}"
 #	SEED=$(($i * 20 + 100))
 	SEED=""
-	mkdir $DIR
-	cp `dirname $0`/runOnHydra.pbs $DIR/runOnHydra-no$i.pbs
-	sed -i "s!DIR!${DIR}!" $DIR/runOnHydra-no$i.pbs
-	sed -i "s!THREADS!${THREADS}!" $DIR/runOnHydra-no$i.pbs
-	sed -i "s!SEED!${SEED}!" $DIR/runOnHydra-no$i.pbs
-	cd $DIR
-	pwd >> $JOBSLOG
-	qsub $DIR/runOnHydra-no$i.pbs >> $JOBSLOG
+	mkdir ${DIR}
+	QSUBFILE=${DIR}/runOnHydra-no${i}.pbs
+	cp ${QSUB_TEMPLATE} ${QSUBFILE}
+	sed -i "s!DIR!${DIR}!" ${QSUBFILE}
+	sed -i "s!THREADS!${THREADS}!" ${QSUBFILE}
+	sed -i "s!SEED!${SEED}!" ${QSUBFILE}
+	cd ${DIR}
+	pwd >> ${JOBSLOG}
+	qsub ${QSUBFILE} >> ${JOBSLOG}
 	cd -
 done
 
-date >> $JOBSLOG
+date >> ${JOBSLOG}
